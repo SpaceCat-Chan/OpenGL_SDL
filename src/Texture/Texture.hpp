ca6dbb1e0ca35aa16eefe13bfc166c0e81af950c
SpaceCat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2020-03-10 14:56:04 +0100

    oops

---
+++ src/Texture/Texture.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <SDL.h>
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <glm/ext.hpp>
+
+
+/**
+ * \brief a class that manages a texture
+ */
+class Texture {
+    
+};
